eab43fa9186db8891f3a0ab0f0cb42dc32e02c9d
KeyvanKhezri <dear.keyvan@gmail.com>
2020-07-06 12:39:08 +0100

    generating a controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
-=begin
+
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  resources :articles
+  resources :articles do
+  	resources :comments
+  end
   
   root 'welcome#index'
-=end
 
-resources :articles do
-	resources :comments
+
 end
